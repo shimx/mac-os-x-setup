3ccb1c0b9a11d4e7fd88a1ec37a208bf4bc89c47
shimx <shimura.32@gmail.com>
2016-07-18 23:19:24 +0900

    initial

---
+++ bin/hosts.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import json
+import os
+
+
+def main():
+    python_interp = os.popen('which python').read().rstrip()
+
+    # python 3
+    print json.dumps({
+        'localhost': {
+            'hosts': [
+                '127.0.0.1'
+            ]
+        },
+        '_meta': {
+            'hostvars': {
+                '127.0.0.1': {
+                    'ansible_python_interpreter': python_interp,
+                    'ansible_user_dir': os.environ['HOME'],
+                    'ansible_user_id': os.environ['USER']
+                    }
+                }
+            }
+        })
+
+
+if __name__ == '__main__':
+    main()

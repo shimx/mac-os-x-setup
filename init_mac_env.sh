f3d72e4e416a89b2eeb406f69d5774884990669f
shimx <shimura.32@gmail.com>
2021-11-27 01:05:26 +0900

    mac用の設定のshellscript追加

---
+++ init_mac_env.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -ue
+
+# キー設定
+defaults write -g KeyRepeat -int 1
+defaults write -g InitialKeyRepeat -int 13
+
+# 拡張子
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# 隠しファイル表示
+defaults write com.apple.finder AppleShowAllFiles TRUE
+
+# パスバー表示
+defaults write com.apple.finder ShowPathbar -bool true
+
+# ステータスバー表示
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# ~/Library ディレクトリを見えるようにする
+chflags nohidden ~/Library
+
+# /Volumes ディレクトリを見えるようにする
+sudo chflags nohidden /Volumes
+
+# Safariデバッグメニュー
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+
+# 再起動
+killall Finder
+killall -kill SystemUIServer
